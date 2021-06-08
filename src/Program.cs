39af4979245ece6b2ab50f3d5584f6dcd2124d58
Chris Sienkiewicz <chsienki@microsoft.com>
2021-06-08 14:48:02 -0700

    Save images to azure blob, and don't redownload unless changed

--- src/Program.cs
+++ src/Program.cs
@@ -4,61 +4,74 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Azure.Storage.Blobs;
 using Mustache;
 
-string rootDir = args.Length > 0 ? args[0] : "..\\.";
-var client = new HttpClient();
+string rootDir = args[0];
+string azureConnection = args[1];
+bool dryRun = args.Length > 2 ? true : false;
 
+var client = new HttpClient();
+var blobContainer = new BlobContainerClient(azureConnection, "img");
+blobContainer.CreateIfNotExists();
+
+var template = File.ReadAllText(Path.Combine(rootDir, "template.html"));
 var options = new JsonSerializerOptions() { AllowTrailingCommas = true };
 var petData = JsonSerializer.Deserialize<Pet[]>(File.ReadAllText(Path.Combine(rootDir, "pets.json")), options).OrderBy(p => p.name);
 
 var updatedPets = new List<Pet>();
 foreach (var pet in petData)
 {
-    updatedPets.Add(pet with { img = await UploadIfNotExists(pet.img) });
+    updatedPets.Add(pet with { img = await UploadIfNotExists(pet) });
 }
 
 var rendered = Template.Compile(template).Render(new { pets = updatedPets });
 File.WriteAllText(Path.Combine(rootDir, "index.html"), rendered);
 
-async Task<string> UploadIfNotExists(string url)
+async Task<string> UploadIfNotExists(Pet pet)
 {
-    string b64FileName = Convert.ToBase64String(Encoding.UTF8.GetBytes(url));
-    // if (!blobExists){
+    if (string.IsNullOrWhiteSpace(pet.img))
+        return "";
 
-    // do conversion and upload
-    // }
-    try
+    string b64FileName = Convert.ToBase64String(Encoding.UTF8.GetBytes(pet.img));
+    var blobClient = blobContainer.GetBlobClient(b64FileName);
+
+    if (!blobClient.Exists())
     {
-        var imgData = await client.GetAsync(url);
-        var resizedImage = await imgData.Content.ReadAsByteArrayAsync();
-        //await putBlobClientAsync(resizedImage);
-    }
-    catch (Exception)
-    {
-        var imgData = await getData(client, pet, retryCountOnFailure: 3);
-        string b64 = Convert.ToBase64String(await imgData.Content.ReadAsByteArrayAsync());
-        b64Pets.Add(pet with { img = "data:image/jpeg;base64," + b64 });
+        try
+        {
+            var imgStream = await getData(client, pet, retryCountOnFailure: 3);
+            if (!dryRun)
+            {
+                blobClient.Upload(imgStream);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Couldn't add pet {pet.name}. Error was " + e.ToString());
+        }
     }
 
-    return b64Pets;
+    return blobClient.Uri.AbsoluteUri.ToString();
 }
 
-async static Task<HttpResponseMessage> getData(HttpClient client, Pet pet, int retryCountOnFailure)
+async static Task<Stream> getData(HttpClient client, Pet pet, int retryCountOnFailure)
 {
     for (int i = 0; i < retryCountOnFailure; i++)
     {
         try
         {
-            return await client.GetAsync(pet.img);
+            var result = await client.GetAsync(pet.img);
+            result.EnsureSuccessStatusCode();
+            return await result.Content.ReadAsStreamAsync();
         }
         catch (HttpRequestException e)
         {
             // Possible temporarily network error. Keep retry 'retryCountOnFailure' times before throwing.
-            if (i == retryCountOnFailure -1)
+            if (i == retryCountOnFailure - 1)
             {
-                Console.WriteLine($"Couldn't add pet {pet.name}. Error was " + e.ToString());
                 throw;
             }
         }
